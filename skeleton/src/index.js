90c32712d1200175adfbfb436562953eb599a7bd
App Academy Student <email@email.com>
2022-03-29 17:32:13 -0400

    work on ttt

--- skeleton/src/index.js
+++ skeleton/src/index.js
@@ -1,6 +1,12 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view");  // require appropriate file
+const Game = require("../ttt_node/game"); // require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const container = document.querySelector(".ttt");
+  const view = new View(game, container);
+
+
 });
+
--- skeleton/src/ttt-view.js
+++ skeleton/src/ttt-view.js
@@ -1,13 +1,72 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.squares = [];
+    this.grid = this.setupBoard();
+    el.appendChild(this.grid);
+    this.bindEvents();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const grid = document.createElement("ul");
+    for(let i = 0; i < 9; i++){
+      const li = document.createElement("li");
+      li.classList.add(`${i}`);
+      this.squares.push(li);
+      grid.appendChild(li);
+    }
+    grid.classList.add("grid");
+    // grid.class = "grid";
+    // grid.display = "flex";
+    // grid.height = 900;
+    // grid.width = 900;
+    // grid.flexWrap = "wrap";
+    // grid.color  = "black";
+    
+    return grid;
+  }
   
-  bindEvents() {}
+  bindEvents() {
+    let that = this;
+    this.grid.addEventListener('click', function(e) {
+      that.handleClick(e);
+    }, true);
+    this.grid.removeEventListener('click', this.handleClick);
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    if (e.target.tagName.toLowerCase() === 'li'){
+      // e.stopPropogation();
+      e.preventDefault();
+      let classId = parseInt(e.target.classList.value);
+      let x = Math.floor(classId/3);
+      let y = classId % 3;
+      this.makeMove([x,y], e);
 
-  makeMove(square) {}
+    }
+  }
+
+  makeMove(square, e) {
+    if(!this.game.board.isEmptyPos(square)){
+      alert("Move Not Valid!");
+    }
+    else{
+      this.game.playMove(square);
+      e.target.innerText = this.game.currentPlayer;
+      e.target.classList.add(`${this.game.currentPlayer.toLowerCase()}`);
+    }
+    if(this.game.isOver()){
+      let winner = document.querySelector("#winner");
+      winner.innerText = `You win, ${this.game.currentPlayer}!`;
+      // debugger;
+      this.grid.removeEventListener('click',function(e) {this.handleClick(e);} );
+      for(let i = 0; i < 9; i++){
+        let ele = this.squares[i];
+        // debugger;
+        ele.removeEventListener('click',function(e) {this.handleClick(e);} );
+      }
+    }
+  }
 
 }
 
